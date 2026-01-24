30fdb60fa182688513b6865dcb4b38d3549c2431
Harsh2616 <harshahiremath2006@gmail.com>
2026-01-24 10:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int pos = 0;  // Position to place next non-zero
-        
-        // Move all non-zero elements to the front
+        int pos = 0;  
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] != 0) {
                 nums[pos++] = nums[i];
             }
         }
-        
-        // Fill the remaining positions with zeros
-        while(pos < nums.size()) {
+         while(pos < nums.size()) {
             nums[pos++] = 0;
         }
     }
