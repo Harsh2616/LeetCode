ecf2ff6ef57cc729f497699705637a08fe0425df
Harsh2616 <harshahiremath2006@gmail.com>
2026-01-29 16:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (98.21%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     void cleanSpaces(string &s) {
         int i = 0, j = 0, n = s.size();
-        
         while (j < n) {
             while (j < n && s[j] == ' ') j++; // skip spaces
-            
             while (j < n && s[j] != ' ')
                 s[i++] = s[j++]; // copy word
-            
             while (j < n && s[j] == ' ') j++; // skip spaces
-            
             if (j < n) s[i++] = ' '; // add single space
         }
         s.resize(i);
     }
-    
     string reverseWords(string s) {
         cleanSpaces(s);
         reverse(s.begin(), s.end());
-        
         int start = 0;
         for (int end = 0; end <= s.size(); end++) {
             if (end == s.size() || s[end] == ' ') {
