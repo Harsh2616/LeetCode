f53b1fd8ebd923407d8608df23361395532ea5ab
Harsh2616 <harshahiremath2006@gmail.com>
2026-06-12 19:31:00 +0530

    Time: 1 ms (46.39%) | Memory: 10.1 MB (79.11%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,26 +1,21 @@
-class Solution {
-public:
-    void cleanSpaces(string &s) {
-        int i = 0, j = 0, n = s.size();
-        while (j < n) {
-            while (j < n && s[j] == ' ') j++; // skip spaces
-            while (j < n && s[j] != ' ')
-                s[i++] = s[j++]; // copy word
-            while (j < n && s[j] == ' ') j++; // skip spaces
-            if (j < n) s[i++] = ' '; // add single space
-        }
-        s.resize(i);
-    }
-    string reverseWords(string s) {
-        cleanSpaces(s);
-        reverse(s.begin(), s.end());
-        int start = 0;
-        for (int end = 0; end <= s.size(); end++) {
-            if (end == s.size() || s[end] == ' ') {
-                reverse(s.begin() + start, s.begin() + end);
-                start = end + 1;
-            }
-        }
-        return s;
-    }
+class Solution 
+{
+ public:string reverseWords(string s) 
+ {
+  string ans = "";
+  int i = s.length() - 1;
+  while (i >= 0) 
+  {
+   while (i >= 0 && s[i] == ' ')
+    i--;
+   if (i < 0) break;
+   int j = i;
+   while (i >= 0 && s[i] != ' ')
+    i--;
+   if (!ans.empty())
+    ans += " ";
+   ans += s.substr(i + 1, j - i);
+  }
+  return ans;
+ }
 };
