81b0858331390a97f101cb5dd22810930af249d4
Harsh2616 <harshahiremath2006@gmail.com>
2026-06-19 20:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.42%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,33 +1,27 @@
-class Solution {
-public:
-    string removeDuplicateLetters(string s) {
-        vector<int> lastIndex(26);
-        vector<bool> visited(26, false);
-        
-        // Store last occurrence of each character
-        for(int i = 0; i < s.size(); i++)
-            lastIndex[s[i] - 'a'] = i;
-        
-        string result = "";
-        
-        for(int i = 0; i < s.size(); i++) {
-            char c = s[i];
-            
-            if(visited[c - 'a'])
-                continue;
-            
-            while(!result.empty() && 
-                  result.back() > c && 
-                  lastIndex[result.back() - 'a'] > i) {
-                
-                visited[result.back() - 'a'] = false;
-                result.pop_back();
-            }
-            
-            result.push_back(c);
-            visited[c - 'a'] = true;
-        }
-        
-        return result;
+class Solution 
+{
+ public:string removeDuplicateLetters(string s) 
+ {
+  vector<int> lastIndex(26);
+  vector<bool> visited(26, false);
+  for(int i = 0; i < s.size(); i++)
+    lastIndex[s[i] - 'a'] = i;
+  string result = "";
+  for(int i = 0; i < s.size(); i++) 
+  {
+   char c = s[i];
+   if(visited[c - 'a'])
+    continue;
+   while(!result.empty() && 
+    result.back() > c && 
+    lastIndex[result.back() - 'a'] > i) 
+    {
+     visited[result.back() - 'a'] = false;
+     result.pop_back();
     }
+    result.push_back(c);
+    visited[c - 'a'] = true;
+   }
+   return result;
+  }
 };
